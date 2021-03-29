10d0250a20be1871257df1d21bc10a3a8b91a783
eugeneshark <yevhenii.skakun@gmail.com>
2021-03-29 17:49:20 +0200

    Modified the login and registration urls and views

--- groupproject/users/urls.py
+++ groupproject/users/urls.py
@@ -4,7 +4,10 @@ from django.conf.urls import include
 
 from users import views
 
+app_name = 'users'
+
 urlpatterns = [
   path('login', views.login_view, name='login'),
   path('registration', views.registration_view, name='registration'),
+  path('dashboard', views.dashboard_view, name='dashboard')
 ]
--- groupproject/users/views.py
+++ groupproject/users/views.py
@@ -1,26 +1,36 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
+
 def login_view(request):
-  if request.metod == 'POST':
-    pass
+  if request.method == 'POST':
+    form = AuthenticationForm(data=request.POST)
+    if form.is_valid():
+      return redirect('users:dashboard')
   else:
     form = AuthenticationForm()
   context = {'form': form}
-  return render(request, "users/login", context)
+  return render(request, "users/login.html", context)
+
 
 def registration_view(request):
-  if request.metod == 'POST':
+  if request.method == 'POST':
     form = UserCreationForm(request.POST)
     if form.is_valid():
       form.save()
-
       return redirect('users:dashboard')
   else:
     form = UserCreationForm()
   context = {'form': form}
   return render(request, 'users/registration.html', context)
 
+
 def logout_view(request):
     logout(request)
+
+
+def dashboard_view(request):
+  dashboard_template = "users/dashboard.html"
+  context = {}
+  return render(request, dashboard_template, context)
