fd8bddd7b431d1b5366d63c82a28f47ffc41ffff
eugeneshark <yevhenii.skakun@gmail.com>
2021-03-29 17:32:20 +0200

    Added login and registration view. Added form to registration page

--- groupproject/users/views.py
+++ groupproject/users/views.py
@@ -3,11 +3,24 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
 def login_view(request):
-  pass
-
+  if request.metod == 'POST':
+    pass
+  else:
+    form = AuthenticationForm()
+  context = {'form': form}
+  return render(request, "users/login", context)
 
 def registration_view(request):
- pass
+  if request.metod == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+
+      return redirect('users:dashboard')
+  else:
+    form = UserCreationForm()
+  context = {'form': form}
+  return render(request, 'users/registration.html', context)
 
 def logout_view(request):
     logout(request)
