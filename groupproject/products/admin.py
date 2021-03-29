6edb7ef669eab3455156d1067d02f857c7d513b1
eugeneshark <yevhenii.skakun@gmail.com>
2021-03-29 11:11:24 +0200

    Added articles app

--- groupproject/products/admin.py
+++ groupproject/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from products.models import *
+from products.models import Product
 
-admin.register(Product)
+admin.site.register(Product)
--- groupproject/products/models.py
+++ groupproject/products/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Product(models.Model):
   name = models.CharField('Nazwa produktu', max_length=100)
   caloric_content = models.CharField('Wartość kaloryczna na 100 g prodkuktu', max_length=10)
+
+  def __str__(self):
+    return self.name
+
