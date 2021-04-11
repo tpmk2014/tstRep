5d41bd270df304feb320d6421bf7c801d22a0ad5
eugeneshark <yevhenii.skakun@gmail.com>
2021-04-11 15:36:58 +0200

    Added Article model

--- groupproject/articles/models.py
+++ groupproject/articles/models.py
@@ -1,2 +1,16 @@
 from django.db import models
+import datetime
 
+
+class Articles(models.Model):
+  title = models.CharField(max_length=100, default="")
+  author = models.CharField(max_length=250, default="")
+  section = models.CharField(max_length=50, default="")
+  description = models.TextField(default="")
+  pub_date = models.DateTimeField('date_published', auto_now=True)
+
+  def __str__(self):
+    return self.title
+
+  def save(self, *args, **kwargs):
+    self.pub_date = datetime.datetime.now()
