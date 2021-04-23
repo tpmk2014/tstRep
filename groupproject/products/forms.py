832ad407d6300bf8753ab7349437c47dc9736895
eugeneshark <yevhenii.skakun@gmail.com>
2021-04-23 21:36:39 +0200

    Added form for adding product to user calories

--- groupproject/products/forms.py
+++ groupproject/products/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from products.models import Product
 
 class ProductForm(forms.Form):
-    name = forms.EmailField(label='Nazwa produktu', required=True)
+    name = forms.CharField(label='Nazwa produktu', required=True)
     caloric_content = forms.CharField(label="Wartość kaloryczna na 100g ", required=True)
 
 class CaloriesForm(forms.Form):
-    name = forms.EmailField(label='Nazwa produktu', required=True)
+    name = forms.CharField(label='Nazwa produktu', required=True)
     caloric_content = forms.CharField(label="Wartość kaloryczna na 100g ", required=True)
