408d4ae8ae52de343275e9f078e54c25ca4fb056
eugeneshark <yevhenii.skakun@gmail.com>
2021-05-06 12:27:58 +0200

    Added way to show photos form media

--- groupproject/groupproject/settings.py
+++ groupproject/groupproject/settings.py
@@ -131,5 +131,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "files", 'media')
 MEDIA_URL = '/media/'
--- groupproject/groupproject/urls.py
+++ groupproject/groupproject/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
+from groupproject import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('', include('users.urls')),
     path('articles/', include('articles.urls')),
 ]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
