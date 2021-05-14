8236bb8a38f9c12058292e74a24b405c1419ddeb
tpmk2014 <pilkamtar@wp.pl>
2021-05-14 16:00:34 +0200

    Dodaliśmy stylizacje artykułów, oraz wstepny tekst tych artykułów.

--- groupproject/articles/views.py
+++ groupproject/articles/views.py
@@ -5,7 +5,8 @@ from articles.models import Article, Section
 
 def article_view(request, article_section):
   section_id = Section.objects.get(name=article_section).pk
-  article = Article.objects.filter(pk=section_id)
+  article = Article.objects.filter(section=section_id)
   context = {"article": article}
+  print(article)
   template = "articles/articles.html"
   return render(request, template, context)
